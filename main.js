57d51d844b73178d5bf726ca33c74510addceffa
Soumya Ranjan Padhy <soumyaranjan@rayvector.com>
2024-06-06 10:23:03 +0530

    03-CSS modules

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import "./src/state";
 import "./src/styles/main.css";
-import style from "./src/styles/main.scss?inline";
+import style from "./src/styles/main.css?inline";
+
+import styleA from "./src/styles/styleA.module.css";
+import styleB from "./src/styles/styleB.module.css";
 
 console.log(style);
+
+document.querySelector(".heading").classList.add(styleA.heading);
