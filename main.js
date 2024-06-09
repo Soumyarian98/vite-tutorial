f12b53c2704e253c086fd6944f0072f4ad9179f6
Soumya Ranjan Padhy <soumyaranjan@rayvector.com>
2024-06-09 15:15:48 +0530

    05-Added scss and json import

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 import "./src/state";
-import "./src/styles/main.css";
-import style from "./src/styles/main.css?inline";
 
-import styleA from "./src/styles/styleA.module.css";
-import styleB from "./src/styles/styleB.module.css";
+import "./src/styles/style.scss";
+import names from "./src/assets/names.json";
 
-console.log(style);
-
-document.querySelector(".heading").classList.add(styleA.heading);
+console.log(names);
